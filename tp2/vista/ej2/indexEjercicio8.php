4bfe2ac8319ea4f37d764f42568f4cac546e0ca2
Julia Rossi <rossijulia1999@gmail.com>
2024-08-23 13:00:42 -0300

    cambio minumo tamanio div

--- tp2/vista/ej2/indexEjercicio8.php
+++ tp2/vista/ej2/indexEjercicio8.php
@@ -4,12 +4,12 @@ include_once '../estructura/headerEj2.php';
     <form name="from" method="get" class="needs-validation" novalidate action="./action/actionEjercicio8.php">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col col-md-2 my-2">
+                <div class="col col-md-3 my-2">
                     <input type="number" min="0" class="form-control" id="edad" name="edad" placeholder="Ingrese su edad" required/>
                 </div>
             </div>
             <div class="row justify-content-center">
-                <div class="col col-md-2 my-2">
+                <div class="col col-md-3 my-2">
                     <select name="estudiante" id="estudiante" class="form-select" required>
                         <option value="es estudiante">Soy estudiante</option>
                         <option value="no es estudiante" selected>No soy estudiante</option>
