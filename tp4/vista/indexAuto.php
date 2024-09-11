65d836cd5edc44871b923c2079236fb6490a1fe8
Federico Lores FAI - 2948 <federico.lores@est.fi.uncoma.edu.ar>
2024-09-11 20:17:16 -0300

    estilo

--- tp4/vista/indexAuto.php
+++ tp4/vista/indexAuto.php
@@ -7,9 +7,12 @@ $objAbmAuto = new AbmAuto();
 
 $listaAuto = $objAbmAuto->buscar(null);
 ?>
-<div class="container"><h3>ABM - Auto</h3>
-<a href="autoNuevo.php">Ingresar un auto</a></div>
-<div class="container"><table class="table table-sm">
+<div class="card p-3">
+	<h3>ABM - Auto</h3>
+	<a class="btn btn-primary" href="autoNuevo.php">Ingresar un auto</a>
+</div>
+<div class="container">
+	<table class="table table-sm">
 	<tr>
 		<th scope="col">Patente</th>
 		<th scope="col">Marca</th>
@@ -26,11 +29,10 @@ if(count($listaAuto)>0){
 		echo '<div class="col"><td>'.$objAuto->getMarca().'</td></div>';
 		echo '<div class="col"><td>'.$objAuto->getModelo().'</td></div>';
 		echo '<div class="col"><td>'.$objAuto->getDniDuenio().'</td></div>';
-        echo '<div class="col"><td><a href="autoEditar.php?Patente='.$objAuto->getPatente().'">editar</a></td></div>';
-        echo '<div class="col"><td><a href="accion/abmAuto.php?accion=borrar&Patente='.$objAuto->getPatente().'">borrar</a></td></tr></div></div>'; 
+        echo '<div class="col"><td><a class="btn btn-success" href="autoEditar.php?Patente='.$objAuto->getPatente().'">editar</a></td></div>';
+        echo '<div class="col"><td><a class="btn btn-danger" href="accion/abmAuto.php?accion=borrar&Patente='.$objAuto->getPatente().'">borrar</a></td></tr></div></div>'; 
 	}
 }
-
 ?>
 </table></div>
 <?php include_once './estructura/footer.php';?>
