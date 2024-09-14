497167fc4aae3eeac19674a5f824559c769e0487
Julia Rossi <rossijulia1999@gmail.com>
2024-09-13 23:26:03 -0300

    cambios ver autos
    ahora muestra bien al duenio y tiene confirmacion antes de eliminar el auto

--- vista/tp4/verAutos.php
+++ vista/tp4/verAutos.php
@@ -30,8 +30,8 @@ if(count($listaAuto)>0){
 		</thead>';
 	echo '<tbody class="table-group-divider">';
     foreach($listaAuto as $objAuto){ 
-		$datosPersona = $persona->buscar($objAuto->getDniDuenio())[0];//hay un error por aca y no lo estoy viendo
-		print_r($datosPersona);
+		$dniDuenio ['NroDni'] = $objAuto->getDniDuenio();
+		$datosPersona = $persona->buscar($dniDuenio)[0];
         echo '<tr><td>'.$objAuto->getPatente().'</td>';
 		echo '<td colspan="3">'.$objAuto->getMarca().'</td>';
 		echo '<td>'.$objAuto->getModelo().'</td>';
@@ -39,16 +39,35 @@ if(count($listaAuto)>0){
 		echo '<td colspan="2">'.$datosPersona->getNombre().'</td>';
 		echo '<td colspan="2">'.$datosPersona->getApellido().'</td>';
         echo '<td><a id="editar" class="btn btn-success" href="cambioDuenio.php?Patente='.$objAuto->getPatente().'">cambiar dueño</a></td>';
-        echo '<td><a id="eliminar" class="btn btn-danger" href="../accion/tp4/accionAuto.php?accion=borrar&Patente='.$objAuto->getPatente().'">borrar</a></td></tr>'; 
+		echo '<td><button type="button" class="btn btn-danger" onclick="confirmarBorrar(\''.$objAuto->getPatente().'\', \'#confirmarEliminarAuto\',
+			\'eliminarAuto\', \'insertarPatente\', \'../accion/tp4/accionAuto.php?accion=borrar&Patente=\')">borrar</button></td></tr>';
 	}
 	echo "</tbody></table>";
 }else{
 	echo '<div class="container text-center">No hay autos cargados</div>';
 }
-?></div>
-<div class="container text-center mb-3">
-<a class="btn btn-primary p-2" href="nuevoAuto.php">Ingresar un auto</a>
-<a class="btn btn-primary p-2" href="buscarAuto.php">Buscar un auto</a>
+?>
+<div class="modal fade" tabindex="-1" id="confirmarEliminarAuto">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="modal-title fs-5">Eliminar de la base de datos</h1>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<div class="modal-body">
+				<span>Esta seguro que desea eliminar de la base de datos a la persona con DNI: <span class="text-body bg-warning text-decoration-none px-1" id="insertarPatente"></span></span>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+				<a id="eliminarAuto" class="btn btn-success">Confirmar eliminacion</a>
+			</div>
+		</div>
+  </div>
 </div>
+	<div class="container text-center mb-3">
+		<a class="btn btn-primary p-2" href="nuevoAuto.php">Ingresar un auto</a>
+		<a class="btn btn-primary p-2" href="buscarAuto.php">Buscar un auto</a>
+	</div>
 </div>
+
 <?php include_once '../estructura/footer.php';?>
