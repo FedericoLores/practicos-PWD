fef015171650269abe8278e41d35f6455cfc7b6a
Federico Lores FAI - 2948 <federico.lores@est.fi.uncoma.edu.ar>
2024-09-17 00:17:32 -0300

    detalle footer

--- vista/estructura/footer.php
+++ vista/estructura/footer.php
@@ -1,7 +1,7 @@
             <footer class="bg-body-secondary py-1 mt-auto">
                 <div class="container text-center">
-                    <p class="display-6">Programacion Web Dinámica</p>
-                    <p>Tecnicatura en Desarrollo Web</p>
+                    <p class="display-6 fw-semibold">Programacion Web Dinámica</p>
+                    <p class="fw-semibold">Tecnicatura en Desarrollo Web</p>
                 </div>
             </footer>
     </body>
